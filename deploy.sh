60bca8d37ef81c6e95983b764189b6ec1a900548
Nick Clyde <nick@clyde.tech>
2021-01-19 10:31:29 -0800

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 git pull
 npm install
 gem install bundler
 bundle install
-npm run build
+npm run setup
+npm run build:$1
 
-if [ "$1" == "demo" ]; then
-  echo "Deploying the static landing page to $1"
-  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&response_type=token_id_token&scope=openid&scope=simple_report&redirect_uri=https://simplereport.cdc.gov/app&nonce=thisisnotsafe&state=thisisbogus,https://demo.simplereport.org/app,' _site/index.html
-  az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportdemo --destination-path '/'
-elif [ "$1" == "beta" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
-fi
+case "$1" in
+  demo|dev|stg|test)
+    echo "Deploying the static landing page to $1"
+    az storage blob upload-batch -s _site/ -d '$web' --account-name simplereport${1}app --destination-path '/'
+    ;;
+  beta)
+    echo "Deploying the static landing page to $1"
+    az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
+    ;;
+esac
